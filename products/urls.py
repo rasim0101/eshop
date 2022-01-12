3517548059973f1eb369611a38410f7f2993710d
coddingp <theodore!seriakov@gmail.com>
2022-01-12 17:53:15 +0600

    Изменили юрлс

---
+++ products/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+from products import views
+
+urlpatterns = [
+    path('', views.product_list, name='product_list'),
+    path('detail', views.product_detail, name='product_detail'), 
+]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
-def homepage(request):
-	return render(request, 'products/homepage.html')
+def product_list(request):
+	return render(request, 'products/product_list.html')
+
+
+def product_detail(request):
+	return render(request, 'products/product_detail.html')
