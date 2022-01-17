e9420675b99233bad1207434bf21b63682c7f3ce
coddingp <theodore!seriakov@gmail.com>
2022-01-17 19:57:32 +0600

    Добавил форму создания

--- products/forms.py
+++ products/forms.py
@@ -1,4 +1,7 @@
+from django.forms import ModelForm
+from .models import Product
 
-#сделать model form
-
-# fields = '__all__'
+class ProductForm(ModelForm):
+	class Meta:
+		model = Product 
+		fields = "__all__"
--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ProductForm
 
 def product_list(request):
 	return render(request, 'products/product_list.html')
@@ -8,4 +9,15 @@ def product_detail(request):
 	return render(request, 'products/product_detail.html')
 
 def create_product(request):
-	return render(request, 'products/create_product.html')
+	if request.method == 'POST':
+		form = ProductForm(request.POST, request.FILES)
+		if form.is_valid():
+			product = form.save()
+			return redirect('product_list')
+	else:
+		form = ProductForm()
+	context = {
+		'form': form
+	}
+	return render(request, 'products/create_product.html', context)
+
